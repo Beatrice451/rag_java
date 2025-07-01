3f8498ebeac19c19fbe4feb8eb078464389e33cf
BeatriceLaplan <beatricelaplan@gmail.com>
2025-07-01 22:43:36 +0300

    test: add unit and integration tests for GitRepository class

---
+++ src/test/java/com/beatrice/rag/utils/GitRepositoryTest.java
@@ -0,0 +1,104 @@
+package com.beatrice.rag.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GitRepositoryTest {
+    private final String testRepo = "https://github.com/octocat/Hello-World.git";
+    private GitRepository repo;
+
+    @TempDir
+    Path tempDir;
+
+
+    @BeforeEach
+    public void setUp() {
+        repo = new GitRepository(testRepo, tempDir);
+    }
+
+
+    @Tag("unit")
+    @Test
+    public void testGetApiUrl() {
+        assertEquals("https://api.github.com/repos/octocat/Hello-World", repo.getApiUrl());
+    }
+
+    @Tag("unit")
+    @Test
+    public void testExtractRepoName() {
+        assertEquals("Hello-World", repo.getRepoName());
+    }
+
+    @Tag("unit")
+    @Test
+    public void testNameGitStripping() {
+        assertEquals("https://github.com/octocat/Hello-World", repo.getRepoUrl());
+    }
+
+
+    /**
+     * Check {@code cloneRepo} method.
+     * <p>
+     * In this test {@code cloneRepo} called and verifies that the repository is cloned successfully.
+     * </p>
+     */
+    @Tag("integration")
+    @Test
+    public void testRepoClone() {
+        repo.cloneRepo();
+        assertTrue(Files.exists(repo.getRepoLocalPath()));
+        assertTrue(repo.isCloned());
+    }
+
+    /**
+     * Check {@code getInfo} method with valid fields.
+     * <p>
+     * In this test {@code getInfo} called with an array of fields {@code {"id", "name", "full_name"}}
+     * * and verifies that the returned map contains the expected keys and corresponding values.
+     * </p>
+     */
+    @Tag("integration")
+    @Test
+    public void testGetInfoValidFields() {
+        String[] fields = {"id", "name", "full_name"};
+        Set<String> fieldsSet = new HashSet<>(Set.of(fields));
+
+        Map<String, Object> info = repo.getInfo(fields);
+        Map<String, Object> expected = new HashMap<>();
+        expected.put("id", 1296269);
+        expected.put("name", "Hello-World");
+        expected.put("full_name", "octocat/Hello-World");
+        assertEquals(expected, info);
+        assertEquals(fieldsSet, info.keySet());
+
+    }
+
+    /**
+     * Same as the test above, but invalid field name is passed ({@code "invalid field name"}).
+     * <p>
+     * Check that field with invalid name is ignored.
+     * </p>
+     */
+    @Tag("integration")
+    @Test
+    public void testGetInfoInvalidFields() {
+        String[] fields = {"invalid field name", "id", "name"};
+        Map<String, Object> info = repo.getInfo(fields);
+        Map<String, Object> expected = new HashMap<>();
+        expected.put("id", 1296269);
+        expected.put("name", "Hello-World");
+        assertEquals(expected, info);
+    }
+
+}
