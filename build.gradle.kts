8b74078efa6d21728818faefefcba9fc5f78d8c1
BeatriceLaplan <beatricelaplan@gmail.com>
2025-07-14 15:33:34 +0300

    feat(ChunkDao): implement retrieveRelevantChunks() method
    retrieveRelevantChunks() method uses the <=> operator from the pgvector extension to find n relevant embeddings in the database, based on cosine similarity

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    runtimeOnly("org.postgresql:postgresql:42.7.7")
+    implementation("org.postgresql:postgresql:42.7.7")
     implementation("org.eclipse.jgit:org.eclipse.jgit:7.3.0.202506031305-r")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
     testImplementation("org.slf4j:slf4j-nop:2.0.12")
--- src/main/java/com/beatrice/rag/database/ChunkDao.java
+++ src/main/java/com/beatrice/rag/database/ChunkDao.java
@@ -1,13 +1,16 @@
 package com.beatrice.rag.database;
 
+import com.beatrice.rag.repositoryprocessor.chunkembedder.Embedding;
 import com.beatrice.rag.repositoryprocessor.textchunker.Chunk;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.postgresql.util.PGobject;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.nio.file.Path;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -21,13 +24,15 @@ public class ChunkDao {
 
     public void saveChunk(Chunk chunk) {
         try (PreparedStatement pstmt = connection.prepareStatement(
-                "INSERT INTO embeddings (content_hash, file_path, embedding, content, metadata) VALUES (?, ?, ?, ?, ?) ON CONFLICT DO NOTHING"
+                "INSERT INTO embeddings (content_hash, file_path, embedding, content, metadata, line_start, line_end) VALUES (?, ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING"
         )) {
             pstmt.setString(1, chunk.getContentHash());
             pstmt.setString(2, String.valueOf(chunk.getSource()));
             pstmt.setObject(3, chunk.getEmbedding().getValues());
             pstmt.setString(4, chunk.getText());
             pstmt.setObject(5, mapToJsonString(chunk.getMetadata()), Types.OTHER);
+            pstmt.setInt(6, chunk.getLineStart());
+            pstmt.setInt(7, chunk.getLineEnd());
             if (pstmt.executeUpdate() == 1) {
                 logger.fine("Chunk %s saved to database".formatted(chunk.getContentHash()));
             } else {
@@ -41,6 +46,42 @@ public class ChunkDao {
 
     }
 
+    public List<Chunk> retrieveRelevantChunks(Embedding embeddedQuestion, int limit) {
+        List<Chunk> chunks = new ArrayList<>();
+        try (PreparedStatement pstmt = connection.prepareStatement(
+                "SELECT * FROM embeddings ORDER BY embedding <=> ?::vector LIMIT ?"
+        )) {
+            pstmt.setObject(1, embeddedQuestion.getValues());
+            pstmt.setInt(2, limit);
+            System.out.println(pstmt);
+
+            var res = pstmt.executeQuery();
+            while (res.next()) {
+                String contentHash = res.getString("content_hash");
+                Path source = Path.of(res.getString("file_path"));
+                int lineStart = res.getInt("line_start");
+                int lineEnd = res.getInt("line_end");
+                PGobject pgEmbedding = (PGobject) res.getObject("embedding");
+                Embedding embedding = new Embedding(pgObjectToFloatArray(pgEmbedding));
+                String content = res.getString("content");
+                PGobject pgMetadata = (PGobject) res.getObject("metadata");
+                Map<String, String> metadata = jsonStringToMap(pgMetadata.getValue());
+                Chunk chunk = new Chunk();
+                chunk.setContentHash(contentHash);
+                chunk.setSource(source);
+                chunk.setMetadata(metadata);
+                chunk.setText(content);
+                chunk.setEmbedding(embedding);
+                chunk.setLineStart(lineStart);
+                chunk.setLineEnd(lineEnd);
+                chunks.add(chunk);
+            }
+        } catch (SQLException e) {
+            logger.warning("Exception occurred while selecting vectors from database: " + e);
+        }
+        return chunks;
+    }
+
 
     private String mapToJsonString(Map<?, ?> map) {
         ObjectMapper mapper = new ObjectMapper();
@@ -49,5 +90,29 @@ public class ChunkDao {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Map<String, String> jsonStringToMap(String json) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(json, new TypeReference<>(){});
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private float[] pgObjectToFloatArray(PGobject pGobject) {
+        String value = pGobject.getValue();
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("PGobject is null or empty");
+        }
+        value = value.replaceAll("[\\[\\]]", "");
+        String[] parts = value.split(",");
+        float[] res = new float[parts.length];
+        for (int i = 0; i < parts.length; i++) {
+            res[i] = Float.parseFloat(parts[i]);
+        }
+
+        return res;
     }
 }
